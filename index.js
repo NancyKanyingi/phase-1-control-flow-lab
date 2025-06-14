3492803b6a55af594242cda68bcb66a7c63f063a
Nancy <wangui.kanyingi2015@gmail.com>
2025-06-14 20:36:44 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+let feet=400
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+   if (feet <= 400) {
+    console.log('This one is on me!')
+}else if(feet>=2000){
+  console.log('That will be twenty dollars')
+}else if(200<feet && feet <2500){
+'That will be a toatal of thirty dollars'
+}else{
+  console.log('There will be no ride')
+};
+
+
+
+function ternaryCheckCity(City){
+let cityDestination=(city===Boston)?"Let us go":"I will pass"
+console.log(cityDestination)
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+    switch(tip){
+    case 'generous':
+    console.log('Thank You so much.')
+    case 'not as generous':
+      console.log('Thank You.')
+  
+  break
+  
+default:
+    console.log('Bye.')
+  }
 }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
